3d03c4e88d96de3f790f556dcf7749ef5aaf16a8
Yaashicaa <yaashi25799@gmail.com>
2020-08-02 11:42:58 +0530

    Login Registration

---
+++ Android- Part3/SIH2020/app/src/main/java/com/example/sih2020/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sih2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
